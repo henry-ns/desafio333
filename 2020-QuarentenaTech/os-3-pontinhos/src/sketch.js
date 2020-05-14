5c8c3d2ef6efea894e8ee115ff1d17ff9e5d8aeb
felipefbs <felipeffbs3x@gmail.com>
2020-05-14 14:17:22 -0300

    feat: pixel falling across the board

--- 2020-QuarentenaTech/os-3-pontinhos/src/sketch.js
+++ 2020-QuarentenaTech/os-3-pontinhos/src/sketch.js
@@ -9,6 +9,10 @@ let timeInterval = 1000;
 function setup() {
   createCanvas(boardX * pixelSize, boardY * pixelSize);
   pixel = new Pixel(width / 2 - pixelSize, 0, pixelSize);
+
+  setInterval(() => {
+    pixel.y += pixelSize;
+  }, timeInterval);
 }
 
 function draw() {
