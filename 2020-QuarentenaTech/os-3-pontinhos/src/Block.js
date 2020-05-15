fff333cc3e3fca6ac17cf15f2f404e9cd7db1593
felipefbs <felipeffbs3x@gmail.com>
2020-05-15 15:47:54 -0300

    feat: applying edges to the board

--- 2020-QuarentenaTech/os-3-pontinhos/src/Block.js
+++ 2020-QuarentenaTech/os-3-pontinhos/src/Block.js
@@ -2,7 +2,7 @@ class Block {
   constructor({ x = 0, y = 0, size = BLOCK_SIZE, color = "#fff" } = {}) {
     this.x = x;
     this.y = y;
-    
+
     this.size = size;
     this.color = color;
   }
@@ -14,5 +14,15 @@ class Block {
 
   update() {
     this.y += this.size;
+
+    if (this.y >= height) {
+      this.y = height - BLOCK_SIZE;
+    }
+    if (this.x <= 0) {
+      this.x += BLOCK_SIZE;
+    }
+    if (this.x >= width) {
+      this.x -= BLOCK_SIZE;
+    }
   }
 }
